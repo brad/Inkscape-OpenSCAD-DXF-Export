9690ca20072e56978329b1d19a9a6e4eec19c562
Brad Pitcher <bradpitcher@gmail.com>
2012-11-21 10:23:32 -0800

    add Mac path, pep8 updates

--- inkinkex.py
+++ inkinkex.py
@@ -14,7 +14,7 @@ this class works just like the usual ``Effect`` class.
 
 The script attempts some guesswork to find out where the Inkscape executable
 might be localized (a fact Inkscape doesn’t tell its extensions), but this is
-rather brittle and might not work. If you encounter an 
+rather brittle and might not work. If you encounter an
 ``InkscapeEnvironmentError``, or your extension fails to find the Inkscape
 executable, uncomment the first line of code of the function
 ``_find_inkscape_path`` and provide a hardcoded path for your local Inkscape
@@ -62,12 +62,16 @@ import os
 import sys
 import tempfile
 
-class InkscapeEnvironmentError(EnvironmentError): pass
+
+class InkscapeEnvironmentError(EnvironmentError):
+    pass
+
 
 def _find_inkscape_path(pathlist):
-    # return "/put/a/hardcoded/path/here"
-    for path in ["/usr/bin/inkscape", "/usr/local/bin/inkscape"]:
-        if os.path.exists(path): return path
+    for path in ["/usr/bin/inkscape", "/usr/local/bin/inkscape",
+            "/Applications/Inkscape.app/Contents/Resources/bin/inkscape"]:
+        if os.path.exists(path):
+            return path
     for p in pathlist:
         p = p.lower().replace("\\", "/")
         if "/python/lib" in p:
@@ -75,15 +79,16 @@ def _find_inkscape_path(pathlist):
     raise InkscapeEnvironmentError(
         u"Can't find the path of the Inkscape executable.")
 
+
 class InkEffect(inkex.Effect):
-    
+
     def __init__(self):
         inkex.Effect.__init__(self)
         self.inkscape_path = _find_inkscape_path(sys.path)
-    
+
     def call_inkscape(self, verbs, ids=None):
         u"""Calls Inkscape to perform inkscape operations on the document.
-        
+
         Note that this destroys and recreates self.document, so any node
         you previously collected is no longer valid. Any manipulation you
         performed on self.document previous to calling ``call_inkscape``
@@ -92,56 +97,56 @@ class InkEffect(inkex.Effect):
         since this node belongs to a document no longer valid. It *is* possible
         to store collections of id's, since those are more persistent between
         subsequent calls of ``call_inkscape``.
-        
+
         Note also that ``call_inkscape`` is a rather costly operation: the
         current document is stored as a temporary file on disk, another
         Inkscape process is loaded into memory, this process loads, manipulates
         and saves the temporary file, and then this temporary file is read and
         parsed again to retrieve the DOM tree. It is therefore advisable to
         perform as much Inkscape commands in one go as possible.
-        
+
         To perform an Inkscape command, you have to specify a *verb*, and an id
         of an object on which to perform this action. Both verbs and ids are
         strings. If you have just a single object with a single operation,
         you can specify both ``verbs`` and ``ids`` as a single string,
         respectively.
-        
+
         If you have a bunch of objects on which you want to perform the same
         operation, you can specify ``verbs`` as a single string, and ``ids``
         as a list of strings.
-        
+
         If you have a bunch of objects on which you have to perform a bunch of
         different operations, you can ommit ``ids`` and specify ``verbs`` as
         a list of (verb, id)-tuples.
-        
+
         And finally, to give you the most ammount of flexibility imaginable,
         you can specify your own list of optional arguments to submit to
         Inkscape if you specify ``verbs`` as a single string and ommit ``ids``.
-        
+
         Examples::
-        
+
             def effect(self):
                 # ...
-                
+
                 # Transform a single text element to a group of paths:
                 self.call_inkscape("ObjectToPath", "text1234")
-                
+
                 # Transform several text elements into groups of paths:
                 self.call_inkscape("ObjectToPath", ["text1", "text2", "text3"])
-                
+
                 # Perform several different operations:
                 self.call_inkscape([("EditClone", "path1234"),
                                     ("ObjectToPath", "text5678")])
-                                 
+
                 # Do arbitrary stuff (remove unused definitions, duplicate
                 # a text and convert the initial version to a path):
                 self.call_inkscape(
                     "--vacuum-defs"
                     " --select=text1234 --verb=EditDuplicate"
                     " --verb=ObjectToPath")
-                
+
                 # ...
-                
+
         """
         fd, tmp = tempfile.mkstemp(".svg", text=True)
         try:
@@ -149,21 +154,21 @@ class InkEffect(inkex.Effect):
             cmd = self.inkscape_path + " --file=\"%s\"" % tmp
             if ids:
                 if isinstance(ids, basestring):
-                    cmd += " --select="+ids
-                    cmd += " --verb="+verbs
+                    cmd += " --select=" + ids
+                    cmd += " --verb=" + verbs
                 else:
                     for id in ids:
                         if id:
-                            cmd += " --select="+id
-                            cmd += " --verb="+verbs
+                            cmd += " --select=" + id
+                            cmd += " --verb=" + verbs
             else:
                 if isinstance(verbs, basestring):
                     cmd += " " + verbs
                 else:
                     for tverb, tid in verbs:
                         if tid and tverb:
-                            cmd += " --select="+tid
-                            cmd += " --verb="+tverb
+                            cmd += " --select=" + tid
+                            cmd += " --verb=" + tverb
             cmd += " --verb=FileSave --verb=FileClose"
             try:
                 from subprocess import Popen, PIPE
