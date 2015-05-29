27cb7962d5e6452ab9b6ec81f58ab97a35252ab6
Brad Pitcher <bradpitcher@gmail.com>
2015-05-29 10:53:55 -0700

    start new version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='inkscape-openscad-dxf',
-    version='0.0.1',
+    version='0.0.2dev',
     description='Inkscape OpenSCAD DXF Export',
     long_description=open('README.md').read(),
     author='Brad Pitcher',
